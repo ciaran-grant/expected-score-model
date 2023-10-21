d2f33730bbb65f46cf583ce723b9bc9aaccbc075
Ciaran Grant <ciaran.grant@hotmail.co.uk>
2023-10-21 13:49:20 +1100

    Updated models with new data and python 3.11

--- src/expected_score_model/config.py
+++ src/expected_score_model/config.py
@@ -10,12 +10,12 @@ behind_open_shots_file_path = "/Users/ciaran/Documents/Projects/AFL/git-reposito
 miss_open_shots_file_path = "/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-score-model/data/open_miss_modelling_data.csv"
 
 # Models
-exp_goal_set_model_file_path = '/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-score-model/model_outputs/models/expected_goal_set_v7.joblib'
-exp_behind_set_model_file_path = '/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-score-model/model_outputs/models/expected_behind_set_v5.joblib'
-exp_miss_set_model_file_path = '/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-score-model/model_outputs/models/expected_miss_set_v4.joblib'
-exp_goal_open_model_file_path = '/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-score-model/model_outputs/models/expected_goal_open_v7.joblib'
-exp_behind_open_model_file_path = '/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-score-model/model_outputs/models/expected_behind_open_v6.joblib'
-exp_miss_open_model_file_path = '/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-score-model/model_outputs/models/expected_miss_open_v5.joblib'
+exp_goal_set_model_file_path = '/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-score-model/model_outputs/models/expected_goal_set_v8.joblib'
+exp_behind_set_model_file_path = '/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-score-model/model_outputs/models/expected_behind_set_v6.joblib'
+exp_miss_set_model_file_path = '/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-score-model/model_outputs/models/expected_miss_set_v5.joblib'
+exp_goal_open_model_file_path = '/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-score-model/model_outputs/models/expected_goal_open_v8.joblib'
+exp_behind_open_model_file_path = '/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-score-model/model_outputs/models/expected_behind_open_v7.joblib'
+exp_miss_open_model_file_path = '/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-score-model/model_outputs/models/expected_miss_open_v6.joblib'
 
 # Preprocessors
 exp_goal_set_preprocessor_file_path = "/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-score-model/model_outputs/preprocessors/set_goal_preproc.joblib"
