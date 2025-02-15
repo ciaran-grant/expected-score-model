5291dd3cb72998d849adfae2f969f0b24e4cecc2
Ciaran Grant <ciaran.grant@hotmail.co.uk>
2025-02-15 21:08:25 +1100

    Make sure predict endpoint works first

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from expected_score_model.predict import predict_xscore_from_chains
-from expected_score_model.visualisation.plot_team_rolling_averages import create_team_rolling, plot_team_rolling_ax, plot_all_team_rolling_figure
+# from expected_score_model.visualisation.plot_team_rolling_averages import create_team_rolling, plot_team_rolling_ax, plot_all_team_rolling_figure
 from AFLPy.AFLData_Client import load_data, upload_data
 
 import matplotlib.pyplot as plt
@@ -20,42 +20,42 @@ def predict(ID=None):
     
     return shots.to_json(orient='records')
 
-@app.route("/model/expectedscore/plot_team_rolling_xscore", methods=["GET", "POST"])
-def plot_team_rolling_xscore():
-    team = request.args.get('team')
-    window = int(request.args.get('window', 10))
-    years = request.args.getlist('years', type=int)
+# @app.route("/model/expectedscore/plot_team_rolling_xscore", methods=["GET", "POST"])
+# def plot_team_rolling_xscore():
+#     team = request.args.get('team')
+#     window = int(request.args.get('window', 10))
+#     years = request.args.getlist('years', type=int)
 
-    # Load shots with xscore
-    shots = load_data(Dataset_Name="CG_Expected_Score")
-    shots['Year'] = shots['Match_ID'].apply(lambda x: int(x.split("_")[1]))
-    shots['Round'] = shots['Match_ID'].apply(lambda x: x.split("_")[2])
+#     # Load shots with xscore
+#     shots = load_data(Dataset_Name="CG_Expected_Score")
+#     shots['Year'] = shots['Match_ID'].apply(lambda x: int(x.split("_")[1]))
+#     shots['Round'] = shots['Match_ID'].apply(lambda x: x.split("_")[2])
 
-    team_rolling = create_team_rolling(shots, team, window, metric='xscore')
-    fig, ax = plt.subplots()
-    ax = plot_team_rolling_ax(ax, team, team_rolling, annotate=True, years=years)
+#     team_rolling = create_team_rolling(shots, team, window, metric='xscore')
+#     fig, ax = plt.subplots()
+#     ax = plot_team_rolling_ax(ax, team, team_rolling, annotate=True, years=years)
 
-    img = io.BytesIO()
-    fig.savefig(img, format='png')
-    img.seek(0)
-    return send_file(img, mimetype='image/png')
+#     img = io.BytesIO()
+#     fig.savefig(img, format='png')
+#     img.seek(0)
+#     return send_file(img, mimetype='image/png')
 
-@app.route("/model/expectedscore/plot_all_team_rolling_xscore", methods=["GET", "POST"])
-def plot_all_team_rolling_xscore():
-    window = int(request.args.get('window', 10))
-    years = request.args.getlist('years', type=int)
+# @app.route("/model/expectedscore/plot_all_team_rolling_xscore", methods=["GET", "POST"])
+# def plot_all_team_rolling_xscore():
+#     window = int(request.args.get('window', 10))
+#     years = request.args.getlist('years', type=int)
 
-    # Load shots with xscore
-    shots = load_data(Dataset_Name="CG_Expected_Score")
-    shots['Year'] = shots['Match_ID'].apply(lambda x: int(x.split("_")[1]))
-    shots['Round'] = shots['Match_ID'].apply(lambda x: x.split("_")[2])
+#     # Load shots with xscore
+#     shots = load_data(Dataset_Name="CG_Expected_Score")
+#     shots['Year'] = shots['Match_ID'].apply(lambda x: int(x.split("_")[1]))
+#     shots['Round'] = shots['Match_ID'].apply(lambda x: x.split("_")[2])
 
-    fig = plot_all_team_rolling_figure(shots, window, years)
+#     fig = plot_all_team_rolling_figure(shots, window, years)
 
-    img = io.BytesIO()
-    fig.savefig(img, format='png')
-    img.seek(0)
-    return send_file(img, mimetype='image/png')
+#     img = io.BytesIO()
+#     fig.savefig(img, format='png')
+#     img.seek(0)
+#     return send_file(img, mimetype='image/png')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8001, debug=False)
